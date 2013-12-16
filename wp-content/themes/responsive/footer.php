fa32560550aa70e39bb0a3af8e1b2be0160e3825
Lauren McCarthy <laurmccarthy@gmail.com>
2013-12-16 04:19:38 -0800

    init

---
+++ wp-content/themes/responsive/footer.php
@@ -0,0 +1,62 @@
+<?php
+
+// Exit if accessed directly
+if( !defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+/**
+ * Footer Template
+ *
+ *
+ * @file           footer.php
+ * @package        Responsive
+ * @author         Emil Uzelac
+ * @copyright      2003 - 2013 ThemeID
+ * @license        license.txt
+ * @version        Release: 1.2
+ * @filesource     wp-content/themes/responsive/footer.php
+ * @link           http://codex.wordpress.org/Theme_Development#Footer_.28footer.php.29
+ * @since          available since Release 1.0
+ */
+
+/*
+ * Globalize Theme options
+ */
+global $responsive_options;
+$responsive_options = responsive_get_options();
+?>
+<?php responsive_wrapper_bottom(); // after wrapper content hook ?>
+</div><!-- end of #wrapper -->
+<?php responsive_wrapper_end(); // after wrapper hook ?>
+</div><!-- end of #container -->
+<?php responsive_container_end(); // after container hook ?>
+
+<div id="footer" class="clearfix">
+	<?php responsive_footer_top(); ?>
+
+	<div id="footer-wrapper">
+
+		<?php get_sidebar( 'footer' ); ?>
+
+		<div class="grid col-940">
+
+
+
+
+		</div>
+		<!-- end of col-940 -->
+		<?php get_sidebar( 'colophon' ); ?>
+
+
+
+	</div>
+	<!-- end #footer-wrapper -->
+
+	<?php responsive_footer_bottom(); ?>
+</div><!-- end #footer -->
+<?php responsive_footer_after(); ?>
+
+<?php wp_footer(); ?>
+</body>
+</html>
